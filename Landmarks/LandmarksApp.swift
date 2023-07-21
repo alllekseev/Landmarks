5d66c486627f5035b247b8a3e32c06243db0a8db
Alekseev Oleg <01.wftp@gmail.com>
2023-07-21 18:36:14 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Олег Алексеев on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
