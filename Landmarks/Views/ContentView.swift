94c84f6a22eadc795a6c4e0f6f16158956df1647
Alekseev Oleg <01.wftp@gmail.com>
2023-07-24 10:21:13 +0300

    rest files

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Олег Алексеев on 21.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
